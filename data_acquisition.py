b50684b56bcdb65097e88a5c0183126cef0bfeed
Bass22 <seyebassirou1997@gmail.com>
2024-06-25 10:17:57 +0200

    Update data_acquisition.py
    Event-based routing function for data acquisition

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,11 +1,9 @@
-import base64
 import functions_framework
-import os
-import json
-import ast
-from google.cloud import (storage, bigquery, pubsub)
+from google.cloud import (storage, bigquery)
 import google.cloud.logging
+import os
 import logging
+import json
 
 def env_vars(env_variable):
     return os.environ.get(env_variable, "Specified environment variable" + env_variable + "is not set.")
@@ -28,71 +26,37 @@ def transfer_file(source_project, source_bucket, source_object, source_object_fi
     logging.info(f"""Transfering file {source_object} from project {source_project}'s bucket {source_bucket}
                  to project {destination_project}'s bucket {destination_bucket}'s directory {destination_directory}
                  """)
-
-# Triggered from a message on a Cloud Pub/Sub topic.
-@functions_framework.cloud_event
-def copy_datasources_to_destination_bucket(cloud_event):    
-    #env vars
-    PROJECT_ID = env_vars('PROJECT_ID')
-    SUBSCRIPTION_NAME = env_vars('SUBSCRIPTION_NAME')
-    ROUTING_REFERENTIAL_TABLE_ID = env_vars('ROUTING_REFERENTIAL_TABLE_ID')
-    MAX_BATCH_SIZE = int(env_vars('MAX_BATCH_SIZE'))
-
-    #setup logging and Big Query Client
-    google.cloud.logging.Client(project=PROJECT_ID).setup_logging()
-    client = bigquery.Client()
     
-    logging.info("Triggered with message : " + str(base64.b64decode(cloud_event.data["message"]["data"])))
+@functions_framework.cloud_event
+def copy_source_file_to_destination_bucket(cloud_event):
+    source_bucket = cloud_event.data["bucket"]
+    source_object = cloud_event.data["name"]
+    source_object_filename = cloud_event.data["name"].split("/")[-1]
+    source_project = env_vars("PROJECT_ID")
+    ROUTING_REFERENTIAL_TABLE_ID = env_vars("ROUTING_REFERENTIAL_TABLE_ID")
 
-    #setup subscriber
-    subscriber = pubsub.SubscriberClient()
-    subscription_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_NAME)
+    google.cloud.logging.Client(project=source_project).setup_logging()
 
-    #consuming all messages from PubSub
-    while True:
+ 
+    # reading referential table from Big Query in order to know where to upload the file
+    client = bigquery.Client()
 
-        response = subscriber.pull(
-            request={
-            "subscription": subscription_path,
-            "max_messages": MAX_BATCH_SIZE,
-            }
-        )
+    QUERY = (
+        f"""
+        SELECT * FROM {source_project}.{ROUTING_REFERENTIAL_TABLE_ID} 
+        WHERE REGEXP_CONTAINS('{source_object_filename}', source_file_mask) is true
+        """)
+    query_job = client.query(QUERY)  # API request
+    rows = list(query_job.result())  # Waits for query to finish
 
-        #when no message pulled
-        if not response.received_messages:
-            logging.info('No message pulled from PUB/SUB')
-            break
-
-        def decodeMsgAndComputeQuery(msg):
-            decoded = ast.literal_eval(msg.message.data.decode('utf-8'))
-            source_bucket = decoded["bucket"]
-            source_object = decoded["name"]
-            source_object_filename = source_object.split("/")[-1]
-            query = f"""
-            (
-                SELECT *, "{source_object}" as source_object, "{source_object_filename}" as source_object_filename FROM {PROJECT_ID}.{ROUTING_REFERENTIAL_TABLE_ID}
-                WHERE source_bucket="{source_bucket}" AND REGEXP_CONTAINS('{source_object_filename}', source_file_mask)
-            )
-            """
-            return query
-        
-        query = 'UNION ALL\n'.join(list(map(decodeMsgAndComputeQuery, response.received_messages)))
-        logging.info("Querying Big Query with :\n" + query)
-
-        query_job = client.query(query)  # API request
-        rows = query_job.result()  # Waits for query to finish
-
-        if rows.total_rows >= 1:
-            for row in rows:
-                transfer_file(row["source_project"], row["source_bucket"], row["source_object"], row["source_object_filename"],
-                               row["destination_project"], row["destination_bucket"], row["destination_directory"])
-        else:
-            logging.warn(f"Query result was empty! Please check if there was any source data.")
-
-        ack_ids = [msg.ack_id for msg in response.received_messages]
-        subscriber.acknowledge(
-            request={
-            "subscription": subscription_path,
-            "ack_ids": ack_ids,
-            }
-        )
+    if len(rows) == 1:
+        row = rows[0]
+        logging.info("File mask matched one line : " + str(row))
+        destination_project = row["destination_project"]
+        destination_bucket = row["destination_bucket"]
+        destination_directory = row["destination_directory"]
+        transfer_file(source_project, source_bucket, source_object, source_object_filename, destination_project, destination_bucket, destination_directory)
+    elif len(rows) >= 2:
+        logging.error(f"Error: More than 1 line matched File mask {source_object_filename}")
+    else:
+        logging.error(f"Error: Matching File Mask for {source_object_filename} not found in referential")
